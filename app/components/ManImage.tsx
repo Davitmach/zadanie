6eeaedbe186be757f53dfa979958c31116b32995
Davitmach <wvime30@gmail.com>
2025-10-03 15:05:10 +0400

    32

--- app/components/ManImage.tsx
+++ app/components/ManImage.tsx
@@ -5,7 +5,7 @@ import ManImg from "@/public/man.png";
 export default function ManImage() {
   return (
     <div className="pay-page__man_img relative w-[380px] h-[767px] shrink-0">
-      <Image className="w-full h-full object-cover" src={ManImg} width={124} height={250} alt="Man" />
+      <Image quality={100} className="w-full h-full object-cover" src={ManImg} width={124} height={250} alt="Man" />
       <div
         className="gradient w-full h-[80px] absolute bottom-0 left-0"
         style={{
